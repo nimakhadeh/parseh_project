99e6eb0ee050366b998430212d48a03a7f57acf8
Nima Khadeh <khadehniam@gmail.com>
2026-07-09 00:15:46 +0330

    تکه اول فاز ۱: مدل Property و ادمین آن

--- properties/admin.py
+++ properties/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Property
 
-# Register your models here.
+@admin.register(Property)
+class PropertyAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'transaction_type', 'is_published', 'created_at')
+    list_filter = ('transaction_type', 'is_published')
+    search_fields = ('title', 'address')
+    prepopulated_fields = {'slug': ('title',)}
+    readonly_fields = ('created_at',)
